01eecb87d64bfc8067cd8ef7aae3a2fd46e543cc
itzrossyo <rossespley@gmail.com>
2024-02-25 02:05:21 +0000

    fixed cart showing how many products have bnow been added to the cart

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -15,24 +15,23 @@ for (i = 0; i < updateBtns.length; i++) {
     })
 }
 
-function updateUserOrder(productId, action) {
-    console.log('User is authenticated, sending data...')
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
 
-    let url = '/update_item/'
+		let url = '/update_item/'
 
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken,
-        },
-        body: JSON.stringify({ productId: productId, action: action }),
-    })
-        .then((response) => {
-            return response.json()
-        })
-        .then((data) => {
-            console.log('data:', data)
-            location.reload()
-        })
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			},
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
 }
