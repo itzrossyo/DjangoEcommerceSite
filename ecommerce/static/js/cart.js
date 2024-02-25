9768a730d86c6576e5adfbdf6cfa489f3599f2e3
itzrossyo <rossespley@gmail.com>
2024-02-25 01:34:23 +0000

    working on the js side of this now
    add to cart button is now out puting to console just working on user auth to see if they are reg or now yet

---
+++ ecommerce/static/js/cart.js
@@ -0,0 +1,16 @@
+let updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+
+        console.log('USER:', user)
+        if (user === 'AnonymousUser') {
+            console.log('User is not authenticated')
+        } else {
+            console.log('User is authenticated, sending data...')
+        }
+    })
+}
