73f401ed2f77f483b115389e48eb52955e2d727a
itzrossyo <rossespley@gmail.com>
2024-02-27 22:20:37 +0000

    added cookies for users not loged into so they keep there items in the basket if they leave

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -15,24 +15,56 @@ for (i = 0; i < updateBtns.length; i++) {
     })
 }
 
-function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
+function updateUserOrder(productId, action) {
+    console.log('User is authenticated, sending data...')
 
-		let url = '/update_item/'
+    let url = '/update_item/'
 
-		fetch(url, {
-			method:'POST',
-			headers:{
-				'Content-Type':'application/json',
-				'X-CSRFToken':csrftoken,
-			},
-			body:JSON.stringify({'productId':productId, 'action':action})
-		})
-		.then((response) => {
-		   return response.json();
-		})
-		.then((data) => {
-		    location.reload()
-		});
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            location.reload()
+        });
 }
+
+
+function addCookieItem(productId, action) {
+    console.log('User is not authenticated')
+
+    if (action === 'add') {
+        if (cart[productId] === undefined) {
+            cart[productId] = {'quantity': 1}
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action === 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Item should be deleted')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+}
+
+if (user === 'AnonymousUser') {
+    addCookieItem(productId, action)
+} else {
+    updateUserOrder(productId, action)
+}
+
 
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -14,7 +14,7 @@ def store(request):
     else:
         # Create empty cart for now for non-logged in user
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
 
     products = Product.objects.all()
@@ -30,9 +30,18 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         # Create empty cart for now for non-logged in user
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+            print('CART:', cart)
+
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]['quantity']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
@@ -83,24 +92,27 @@ def updateItem(request):
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
+
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
+
         if total == order.get_cart_total:
             order.complete = True
-            order.save()
-            if order.shipping:
-                ShippingAddress.objects.create(
-                    customer=customer,
-                    order=order,
-                    address=data['shipping']['address'],
-                    city=data['shipping']['city'],
-                    state=data['shipping']['state'],
-                    zipcode=data['shipping']['zipcode'],
-                )
-    else:
-        print('User is not logged in..')
+        order.save()
 
-    return JsonResponse('Payment complete!', safe=False)
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+    else:
+        print('User is not logged in')
+
+    return JsonResponse('Payment submitted..', safe=False)
