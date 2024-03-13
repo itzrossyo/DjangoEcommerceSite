f7e5cb6b4a051eb33ed1a5c532a454713bfcf4f1
itzrossyo <rossespley@gmail.com>
2024-03-13 09:17:48 +0000

    changed rq.text

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-(zatl%azt0z8w^y&j4epm0v8fdmiiyjrkpvmmti!!xsu))or8@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 # Application definition
 
